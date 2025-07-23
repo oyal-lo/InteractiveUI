3fb43500afb0ff8e49e11347b5e6c730f67511fc
Olive Luong <olivee.yan@gmail.com>
2025-07-23 13:58:42 -0700

    everything basic: gray and purple

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -8,16 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var name: String = ""
+    
+    @State private var textTitle = "What is your name?"
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("What's your name?")
+            TextField("Type your name here...",
+                      text:$name)
+            .multilineTextAlignment(.center)
+            .font(.title)
+            .border(Color.gray, width:1)
+            Button("Submit Name") {
+            }
+            .font(.title2)
+                .buttonStyle(.borderedProminent)
+                .tint(.purple)
+            
         }
         .padding()
     }
 }
+
 
 #Preview {
     ContentView()
